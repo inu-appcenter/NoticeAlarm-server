db81161e8549eda63a27cdc726bca1d8862711da
seungh1024 <seungh1024@naver.com>
2021-08-16 15:53:18 +0900

    [Update] 응답 상태코드 200으로 업데이트, 키워드 string으로 받도록 업데이트

--- routes/info.js
+++ routes/info.js
@@ -8,37 +8,34 @@ const router = express.Router();
 router.post('/',async(req,res,next)=>{
     console.log(req.body.keyword);
     console.log(req.body.keyword.length)
-    //리스트로 보낸 키워드 길이
-    const length = req.body.keyword.length;
     try{
-        for(var i = 0; i< length; i++){//키워드 길이만큼 로우 생성
-            const students = await Student.findOrCreate({
-                where:{//찾아보고
-                    major:req.body.major,
-                    token:req.body.token,
-                    keyword:req.body.keyword[i]
-                },
-                defaluts:{//없으면 defaults 값으로 생성
-                    major:req.body.major,
-                    token:req.body.token,
-                    keyword:req.body.keyword[i]
-                }
-                
-            })
-            console.log(students);
+        //학생정보 생성 ->찾아보고 없으면
+        const students = await Student.findOrCreate({
+            where:{//찾아보고
+                major:req.body.major,
+                token:req.body.token,
+                keyword:req.body.keyword
+            },
+            defaluts:{//없으면 defaults 값으로 생성
+                major:req.body.major,
+                token:req.body.token,
+                keyword:req.body.keyword
+            }
+            
+        })
+        console.log(students);
 
-            //키워드 테이블에서 찾고 없으면 생성함
-            const keyword = await Keyword.findOrCreate({
-                where:{//키워드가 있는지 찾아보고
-                    word:req.body.keyword[i]
-                },
-                defaults:{//없으면 defaults 값으로 그 키워드를 생성
-                    word:req.body.keyword[i]
-                }
-            })
-            console.log(keyword);
-        }
-        res.status(200).json('Student,Keyword Ok');
+        //키워드 테이블에서 찾고 없으면 생성함
+        const keyword = await Keyword.findOrCreate({
+            where:{//키워드가 있는지 찾아보고
+                word:req.body.keyword
+            },
+            defaults:{//없으면 defaults 값으로 그 키워드를 생성
+                word:req.body.keyword
+            }
+        })
+        console.log(keyword);
+        res.status(200).json('200');
     }catch(error){
         console.error(error);
         next(error);
