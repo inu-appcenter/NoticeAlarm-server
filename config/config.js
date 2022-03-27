98c47a97ebb636accd46f843e5bfd110d6c6f161
potados99 <potados99@gmail.com>
2022-03-27 15:51:55 +0900

    fix: DB 이름은 소문자로

--- config/config.js
+++ config/config.js
@@ -4,21 +4,21 @@ module.exports={
   "development": {
     "username": process.env.SEQUELIZE_USERNAME,
     "password": process.env.SEQUELIZE_PASSWORD,//.env 파일에 비밀번호 설정
-    "database": "Notice",//db 이름
+    "database": "notice",//db 이름
     "host": process.env.SEQUELIZE_HOST,
     "dialect": "mysql"
   },
   "test": {
     "username": process.env.SEQUELIZE_USERNAME,
     "password": process.env.SEQUELIZE_PASSWORD,
-    "database": "Notice",
+    "database": "notice",
     "host": process.env.SEQUELIZE_HOST,
     "dialect": "mysql"
   },
   "production": {
     "username": process.env.SEQUELIZE_USERNAME,
     "password": process.env.SEQUELIZE_PASSWORD,
-    "database": "Notice",
+    "database": "notice",
     "host": process.env.SEQUELIZE_HOST,
     "dialect": "mysql"
   }
