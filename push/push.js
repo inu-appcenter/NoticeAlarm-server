81a2090f81a187e1c1a86c6c661aa92cc5f1ae22
potados99 <potados99@gmail.com>
2022-03-27 15:11:23 +0900

    refactor: 파이어베이스 인증파일은 하드코딩 대신 GOOGLE_APPLICATION_CREDENTIALS 환경변수로 전달

--- push/push.js
+++ push/push.js
@@ -1,25 +1,21 @@
 const admin = require("firebase-admin");
 const request = require("request");
 //파이어베이스 프로젝트에서 만든 키값이 있음
-var serviceAccount = require("../key/noticealarm-2271d-firebase-adminsdk-j9f5e-bddea59dd0.json");
-
 
 //파이어베이스 연결
-admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount)
-});
+admin.initializeApp();
 
 //푸쉬 알람 전송을 담당하는 함수
 const pushFunction = async(pushData,link,title,next)=>{
     const target_token =pushData.token;
-    
+
     console.log(link);
     console.log(title);
     console.log(pushData.token);
     console.log(pushData.keyword);
 
     const options = {
-        uri:'https://fcm.googleapis.com/fcm/send', 
+        uri:'https://fcm.googleapis.com/fcm/send',
         method: 'POST',
         headers: {
             "Content-Type": "application/json",
@@ -63,14 +59,14 @@ const pushFunction = async(pushData,link,title,next)=>{
     //         console.log('Error sending message:',error)
     //     })
 }
-  
-  
+
+
   //여러기기에 메세지 전송 ->sendMulticast(message)
   // admin.messaging().sendMulticast(message)
   //   .then((response) => {
   //     console.log(response.successCount + ' messages were sent successfully');
   //   });
-  
+
   //푸쉬 알림이 실패하는 경우가 좀 있다고 함. 그 때 아래처럼 실패한 것들 받아서 다시 배열에 넣어주는 식으로
   // admin.messaging().sendMulticast(message)
   //   .then((response) => {
@@ -84,5 +80,5 @@ const pushFunction = async(pushData,link,title,next)=>{
   //       console.log('List of tokens that caused failures: ' + failedTokens);
   //     }
   //   });
-  
+
   module.exports = pushFunction;
